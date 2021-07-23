efd6030c901f513ab9ff9af563c909d013563d7a
Mike Mwanje <mwanjemike767@gmail.com>
2021-07-23 12:29:36 +0300

    input area test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,21 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Displays Crane Cloud logo', () => {
-    render(<App />);
-    const logo = screen.getByRole('img');
-    expect(logo).toBeInTheDocument();
-    expect(logo).toHaveAttribute('src', '/logo.svg');
-    expect(logo).toHaveAttribute('alt', 'Logo');
-});
-
 test('Welcomes users', () => {
     render(<App />);
-    const welcomeText = screen.getByText(/Welcome to our Deployment Utility/i);
+    const welcomeText = screen.getByText(/Welcome to the Deployment Utility/i);
     expect(welcomeText).toBeInTheDocument();
 });
 
-test('Continue link', () => {
+test('Has url input', () => {
+    render(<App />);
+    const input = screen.getByRole('input')
+    expect(input).toBeInTheDocument();
+});
+
+test('Captures the image url', () => {
   render(<App />);
   const linkElement = screen.getByText(/Continue/i);
   expect(linkElement).toBeInTheDocument();
