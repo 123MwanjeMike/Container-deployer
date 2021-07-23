150287fe2a2981b785aa120f57ffd4d0ddeeb8bb
Mike Mwanje <mwanjemike767@gmail.com>
2021-07-24 01:50:30 +0300

    resolve missing dependencies

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Index from './Views/index';
 
 test('Welcomes users', () => {
-  render(<App />);
+  render(<Index />);
   const welcomeText = screen.getByText(/Welcome to the Deployment Utility/i);
   expect(welcomeText).toBeInTheDocument();
 });
 
 test('Captures the image url', () => {
-  render(<App />);
+  render(<Index />);
   const linkElement = screen.getByText(/Continue/i);
   expect(linkElement).toBeInTheDocument();
 });
