0a2fdba346a737d93150765f292e3011d04873f3
Mike Mwanje <mwanjemike767@gmail.com>
2021-07-23 12:11:02 +0300

    App test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Displays Crane Cloud logo', () => {
+    render(<App />);
+    const logo = screen.getByRole('img');
+    expect(logo).toBeInTheDocument();
+    expect(logo).toHaveAttribute('src', '/logo.svg');
+    expect(logo).toHaveAttribute('alt', 'Logo');
+});
+
+test('Welcomes users', () => {
+    render(<App />);
+    const welcomeText = screen.getByText(/Welcome to our Deployment Utility/i);
+    expect(welcomeText).toBeInTheDocument();
+});
+
+test('Continue link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Continue/i);
   expect(linkElement).toBeInTheDocument();
 });
+
