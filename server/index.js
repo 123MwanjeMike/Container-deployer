aebc7990d6200fda3bc66c006a609c89fd71e0ee
Mike Mwanje <mwanjemike767@gmail.com>
2021-07-24 18:34:35 +0300

    server index

---
+++ server/index.js
@@ -0,0 +1,8 @@
+import app from './server';
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Application Server is up and running on port ${port}`);
+});
