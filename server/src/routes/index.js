4c4a1dd65b010f869822b50c84970fe3727744ef
Mike Mwanje <mwanjemike767@gmail.com>
2021-07-24 22:00:13 +0300

    for non existing routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,13 @@
-import containerRoutes from './container'
+import containerRoutes from './container';
 
 export default (server) => {
   // Default route
-  server.get('/', (req, res) => {      
-    res.status(200).json({ message: 'Welcome to the deployment utility' })
-  })
-  server.use('/container', containerRoutes)
-}
+  server.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the deployment utility' });
+  });
+  server.use('/container', containerRoutes);
+  // For non existent requests
+  server.all('*', (req, res) => {
+    res.status(404).json({ message: 'Resource not found' });
+  });
+};
